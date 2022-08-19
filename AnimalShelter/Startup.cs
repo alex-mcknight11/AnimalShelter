7bbba2ee432cbd68a91cc8b0e8fd36ca33de94cb
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-08-19 10:52:13 -0700

    add startup and home controller

---
+++ AnimalShelter/Startup.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Http;
+using AnimalShelter.Models;
+using AnimalShelter.Services;
+
+namespace AnimalShelter
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<AnimalShelterContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+            services.AddHttpContextAccessor();
+            services.AddSingleton<IUriService>(o =>
+            {
+                var accessor = o.GetRequiredService<IHttpContextAccessor>();
+                var request = accessor.HttpContext.Request;
+                var uri = string.Concat(request.Scheme, "://", request.Host.ToUriComponent());
+                return new UriService(uri);
+            });
+            services.AddControllers();
+            services.AddApiVersioning( o => {
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new ApiVersion(2,0);
+            });
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "Animal Shelter Api",
+                    Version = "v1",
+                    Description = "An ASP.NET Core Web Api for an Animal Shelter"
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+            services.AddSwaggerDocument(config=>
+            {
+              config.PostProcess = document => 
+              {
+                document.Info.Version="v2";
+                document.Info.Title = "AnimalShelter APi";
+                document.Info.Description = "An ASP.NET Core Web Api for an Animal Shelter";
+                document.Info.Contact = new NSwag.OpenApiContact
+                {
+                  Name = "Alex McKnight",
+                  Email = "alex.mcknight11@gmail.com",
+                  Url ="https://github.com/alex-mcknight11"
+                };
+              };
+            }
+            );
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseOpenApi(); //
+            app.UseSwaggerUi3();//
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+  }
+}
